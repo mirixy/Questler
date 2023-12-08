2add88d8047a26fde3e991f8b7e8ea5141a36f71
Miriam <miriam.lucy@outlook.com>
2023-12-08 16:11:29 +0100

    Initial Commit

---
+++ Cursed/CursedApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursedApp.swift
+//  Cursed
+//
+//  Created by Miriam Renken on 08.12.23.
+//
+
+import SwiftUI
+
+@main
+struct CursedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
